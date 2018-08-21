cd5835373ee2bbc06576273bcf0133e00801732a
Robert Schumann <rs@n-os.org>
2018-08-22 00:29:59 +0200

    implement api handlers, improve index.html

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -10,30 +11,30 @@ import (
 	//"errors"
 
 	"time"
-	//"strconv"
 
+	"github.com/gorilla/mux"
 	. "github.com/gutmensch/goweck/appbase"
+	"github.com/imdario/mergo"
 
 	"github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
-	"github.com/gorilla/mux"
 )
 
 type Alarm struct {
-	ID            bson.ObjectId `bson:"_id,omitempty" json:"_id,omitempty"`
-	Enable        bool          `bson:"enable"        json:"enable"`
-	HourMinute    string        `bson:"hourMinute"    json:"hourMinute"`
-	CreatedAt     time.Time     `bson:"createdAt"     json:"createdAt"`
-	WeekDays      bool          `bson:"weekDays"      json:"weekDays"`
-	WeekEnds      bool          `bson:"weekEnds"      json:"weekEnds"`
-	RaumserverUri string        `bson:"raumserverUri" json:"raumserverUri"`
-	AlarmZone     string        `bson:"alarmZone"     json:"alarmZone"`
-	RadioChannel  string        `bson:"radioChannel"  json:"radioChannel"`
-	VolumeStart   int           `bson:"volumeStart"   json:"volumeStart"`
-	VolumeEnd     int           `bson:"volumeEnd"     json:"volumeEnd"`
-	VolumeIncStep int           `bson:"volumeIncStep" json:"volumeIncStep"`
-	VolumeIncInt  int           `bson:"volumeIncInt"  json:"volumeIncInt"`
-	Timeout       int           `bson:"timeout"       json:"timeout"`
+	ID            bson.ObjectId `bson:"_id,omitempty"           json:"_id,omitempty"`
+	Enable        string        `bson:"enable,omitempty"        json:"enable,omitempty"`
+	HourMinute    string        `bson:"hourMinute,omitempty"    json:"hourMinute,omitempty"`
+	LastModified  string        `bson:"lastModified,omitempty"  json:"lastModified,omitempty"`
+	WeekDays      string        `bson:"weekDays,omitempty"      json:"weekDays,omitempty"`
+	WeekEnds      string        `bson:"weekEnds,omitempty"      json:"weekEnds,omitempty"`
+	RaumserverUri string        `bson:"raumserverUri,omitempty" json:"raumserverUri,omitempty"`
+	AlarmZone     string        `bson:"alarmZone,omitempty"     json:"alarmZone,omitempty"`
+	RadioChannel  string        `bson:"radioChannel,omitempty"  json:"radioChannel,omitempty"`
+	VolumeStart   int           `bson:"volumeStart,omitempty"   json:"volumeStart,omitempty"`
+	VolumeEnd     int           `bson:"volumeEnd,omitempty"     json:"volumeEnd,omitempty"`
+	VolumeIncStep int           `bson:"volumeIncStep,omitempty" json:"volumeIncStep,omitempty"`
+	VolumeIncInt  int           `bson:"volumeIncInt,omitempty"  json:"volumeIncInt,omitempty"`
+	Timeout       int           `bson:"timeout,omitempty"       json:"timeout,omitempty"`
 }
 
 var (
@@ -146,9 +147,9 @@ func pollAlarm() {
 		}
 
 		err := c.Find(bson.M{
-			"hourMinute": fmt.Sprintf("%02d%02d", t.Hour(), t.Minute()),
-			"weekDays":   weekDays,
-			"weekEnds":   weekEnds,
+			"hourMinute": fmt.Sprintf("%02d:%02d", t.Hour(), t.Minute()),
+			"weekDays":   strconv.FormatBool(weekDays),
+			"weekEnds":   strconv.FormatBool(weekEnds),
 		}).One(&result)
 		if err == mgo.ErrNotFound || err != nil {
 			if Debug {
@@ -178,14 +179,14 @@ func main() {
 
 	ensureIndices()
 
-	saveAlarm("1601", true, false, "uuid:C43C1A1D-AED1-472B-B0D0-210B7925000E", "http://mp3channels.webradio.rockantenne.de/alternative")
+	saveAlarm(true, "16:01", true, false, "uuid:C43C1A1D-AED1-472B-B0D0-210B7925000E", "http://mp3channels.webradio.rockantenne.de/alternative")
 
 	// execute or change alarms periodically
 	go pollAlarm()
 
 	// http endpoint for dealing with alarms
-	mux := httpRouter()
-	log.Fatal(http.ListenAndServe(Listen, mux))
+	router := httpRouter()
+	log.Fatal(http.ListenAndServe(Listen, router))
 }
 
 func ensureIndices() {
@@ -203,14 +204,14 @@ func ensureIndices() {
 	Fatal(err)
 }
 
-func saveAlarm(hourMinute string, weekDays bool, weekEnds bool, zoneId string, radioChannel string) {
+func saveAlarm(enable bool, hourMinute string, weekDays bool, weekEnds bool, zoneId string, radioChannel string) {
 	c := Database.C("alarm").With(Database.Session.Copy())
 	err := c.Insert(&Alarm{
-		Enable:        true,
+		Enable:        strconv.FormatBool(enable),
 		HourMinute:    hourMinute,
-		CreatedAt:     time.Now(),
-		WeekDays:      weekDays,
-		WeekEnds:      weekEnds,
+		LastModified:  time.Now().String(),
+		WeekDays:      strconv.FormatBool(weekDays),
+		WeekEnds:      strconv.FormatBool(weekEnds),
 		RaumserverUri: RaumserverUri,
 		RadioChannel:  radioChannel,
 		AlarmZone:     zoneId,
@@ -225,34 +226,104 @@ func saveAlarm(hourMinute string, weekDays bool, weekEnds bool, zoneId string, r
 
 func httpRouter() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/alarm", http.HandlerFunc(AlarmGetHandler)).Methods("GET")
+	r.HandleFunc("/alarms", http.HandlerFunc(AlarmListHandler)).Methods("GET")
+	r.HandleFunc("/alarm", http.HandlerFunc(AlarmCreateHandler)).Methods("POST")
+	r.HandleFunc("/alarm/{id:[0-9a-f]{24}}", http.HandlerFunc(AlarmUpdateHandler)).Methods("POST")
+	r.HandleFunc("/alarm/{id:[0-9a-f]{24}}", http.HandlerFunc(AlarmDeleteHandler)).Methods("DELETE")
+
 	staticFileDirectory := http.Dir("./assets/")
 	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
 	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
 
-	//r.Get("/alarm", http.HandlerFunc(AlarmGetHandler))
-	//r.Post("/alarm", http.HandlerFunc(AlarmPostHandler))
-	//r.Handle("/", http.HandlerFunc(DocumentationHandler))
-
-	//staticFileDirectory := http.Dir("./assets/")
-	//staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
-	//r.Prefix("/assets/").Handl
-
 	return r
 }
 
-func AlarmGetHandler(w http.ResponseWriter, r *http.Request) {
+/*
+func getRequestParam(r *http.Request, key string) (string, error) {
+	keys, ok := r.URL.Query()[key]
+	if !ok || len(keys[0]) < 1 {
+		return "", errors.New("parameter missing")
+	}
+	return keys[0], nil
+}
+*/
+
+func AlarmListHandler(w http.ResponseWriter, r *http.Request) {
 	var results []Alarm
 	c := Database.C("alarm").With(Database.Session.Copy())
 	err := c.Find(bson.M{}).All(&results)
 	Fatal(err)
 	e, err := json.Marshal(results)
 	Log(err)
+	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "%q\n", string(e))
 }
 
-func AlarmPostHandler(w http.ResponseWriter, r *http.Request) {
+func AlarmUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	Fatal(err)
+	var new, old Alarm
+	err = json.Unmarshal(body, &new)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	Fatal(err)
+	vars := mux.Vars(r)
+	c := Database.C("alarm").With(Database.Session.Copy())
+	err = c.FindId(bson.ObjectIdHex(vars["id"])).One(&old)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	err = mergo.Merge(&new, old)
+	Log(err)
+	err = c.UpdateId(bson.ObjectIdHex(vars["id"]), new)
+	if err != nil {
+		Log(err)
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
-func DocumentationHandler(w http.ResponseWriter, r *http.Request) {
+func AlarmDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	c := Database.C("alarm").With(Database.Session.Copy())
+	err := c.RemoveId(bson.ObjectIdHex(vars["id"]))
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func AlarmCreateHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	Fatal(err)
+	var new, result Alarm
+	err = json.Unmarshal(body, &new)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	c := Database.C("alarm").With(Database.Session.Copy())
+	dupAlarm := c.Find(bson.M{
+		"hourMinute": new.HourMinute,
+		"weekDays":   new.WeekDays,
+		"weekEnds":   new.WeekEnds,
+	}).One(&result)
+
+	// bail out early if anything else then NotFound
+	if dupAlarm != mgo.ErrNotFound {
+		w.WriteHeader(http.StatusConflict)
+		return
+	}
+
+	err = c.Insert(&new)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
