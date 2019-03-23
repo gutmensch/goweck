c59297bdd4e44caa8a4b5ec5e71072c7f0fc76e7
Robert Schumann <robert@schumann.link>
2019-03-23 10:06:07 +0100

    add stop overlay

--- index.go
+++ index.go
@@ -317,7 +317,7 @@ var indexHTML = `<!DOCTYPE html>
 			}
 		});
 	}
-	function stopAlarm(id) {
+	function stopAlarm() {
 		$.ajax({
 			url: '/alarm/stop',
 			type: 'POST',
@@ -336,9 +336,94 @@ var indexHTML = `<!DOCTYPE html>
 	  });
 	}
 	</script>
+	<style type="text/css">
+		/* The Modal (background) */
+		.modal {
+		  display: none; /* Hidden by default */
+		  position: fixed; /* Stay in place */
+		  z-index: 1; /* Sit on top */
+		  padding-top: 100px; /* Location of the box */
+		  left: 0;
+		  top: 0;
+		  width: 100%; /* Full width */
+		  height: 100%; /* Full height */
+		  overflow: auto; /* Enable scroll if needed */
+		  background-color: rgb(0,0,0); /* Fallback color */
+		  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+		}
+
+		/* Modal Content */
+		.modal-content {
+		  background-color: #ff0000;
+		  margin: auto;
+		  padding: 20px;
+		  border: 1px solid #888;
+		  width: 80%;
+		}
+
+		/* The Close Button */
+		.close {
+		  color: #dddddd;
+		  float: center;
+		  font-size: 20px;
+		  font-weight: bold;
+		}
+
+		.close:hover,
+		.close:focus {
+		  color: #000;
+		  text-decoration: none;
+		  cursor: pointer;
+		}
+	</style>
+	<script>
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	function checkRunningAlarm() {
+		$.ajax({
+			url: '/alarm/running',
+			type: 'GET',
+			success: function(data) {
+				modal.style.display = "block";
+			}
+		});
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+		stopAlarm();
+		getAlarms();
+	  modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+	    modal.style.display = "none";
+	  }
+	}
+	</script>
+
 </head>
 
-<body onload="getAlarms();populateZoneSelect();populateStreamSelect()">
+<body onload="getAlarms();populateZoneSelect();populateStreamSelect();checkRunningAlarm()">
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">Stop Alarm</span>
+  </div>
+
+</div>
 
 	<h2>Configured Alarms</h2>
 
